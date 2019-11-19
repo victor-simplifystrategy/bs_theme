733e80f166c69e4df13fae9eba8b88ebb723e6b7
Victor Flores <victor@simplifystrategy.com>
2019-11-19 00:50:09 -0500

    theme commit

---
+++ assets/dmr.js
@@ -0,0 +1,11 @@
+if(typeof Spurit === 'undefined') var Spurit = {};
+if(typeof Spurit.Discountmanager === 'undefined') Spurit.Discountmanager = {};
+Spurit.Discountmanager.settings = {
+    enabled: false,
+    badge: {},
+    enabledCollection: false,
+    collectionPageSelector: "",
+    productPageSelector: "",
+    productIds: [],
+    variantIds: []
+};
